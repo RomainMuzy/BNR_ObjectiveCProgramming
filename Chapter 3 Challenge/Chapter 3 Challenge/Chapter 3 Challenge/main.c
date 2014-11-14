af3f820de1a660904d6946719ff0520113465f3f
RomainMuzy <romainmuzy@Romains-MBP.attlocal.net>
2014-11-13 20:35:38 -0600

    Commit of the second program and challenge

---
+++ Chapter 3 Challenge/Chapter 3 Challenge/Chapter 3 Challenge/main.c
@@ -0,0 +1,27 @@
+//
+//  main.c
+//  Chapter 3 Challenge
+//
+//  Created by Romain Muzy on 11/13/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    // Define the two floats
+    float firstVariable;
+    float secondVariable;
+    
+    firstVariable = 3.14;
+    secondVariable = 42.0;
+    
+    double sum = firstVariable+secondVariable;
+    
+    printf("This is the sum: %f \n", sum);
+    
+    
+    
+    return 0;
+}
